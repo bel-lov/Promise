be29b39fb8d64b581910fbe52f4db17ea07d0f70
bel-lov <belou.lyuba@yandex.ru>
2022-11-08 20:28:39 +0300

    first commit

---
+++ src/GameSavingLoader.js
@@ -0,0 +1,17 @@
+import { resolve } from 'path';
+import json from './parser'
+import read from './reader'
+
+export default class GameSavingLoader {
+    static load() {
+        return new Promise((resolve) => {
+            const data = read();
+            data.then((response) => {
+                const value = json(response);
+                return value;
+            }).then((value) => {
+                resolve(value)
+            });
+        });
+    }
+}
